6a9074168004e41075527223fc08011ff4918920
Archer <jordanarcherdev@gmail.com>
2018-06-26 23:58:26 +0100

    first commit

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { User } from '../../models/User'
 
 @Component({
   selector: 'app-user',
@@ -12,49 +14,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./user.component.css']
 })
 
-export class UserComponent {
-// Properties
-  firstName: string;
-  lastName: string;
-  age: number;
-  address;
-
-  foo: any;
-  hasKids: boolean;
-  numberArray: number[];
-  stringArray: string[];
-  mixedArray: any[];
-  myTuple : [string, number, boolean];
-  unusable: void;
-  u: undefined;
-  n: null;
+export class UserComponent implements OnInit {
+  //Properties
+  user: User;
 
   //Methods
   constructor() {
-    this.firstName = 'John';
-    this.lastName = 'Doe';
-    this.age = 30;
 
-    this.address = {
-      street: '50 Main st',
-      city: 'Boston',
-      state: 'MA'
-    }
-
-    this.foo = 1;
-    this.hasKids = true;
-    this.numberArray = [1,2,3];
-    this.stringArray = ['Hello ', 'World '];
-    this.mixedArray = [true, undefined, 'hello'];
-    this.myTuple = ['hello',1,true];
-    this.unusable = undefined;
-    this.u = undefined;
-    this.n = null;
-    console.log(this.addNumbers(2,3));
 }
 
-  addNumbers(num1:number, num2:number):number {
-    return num1 + num2;
+  ngOnInit() {
+    this.user = {
+      firstName: 'John',
+      lastName: 'Doe',
+      age: 30,
+      address:  {
+        street: '50 Main St',
+        city: 'Boston',
+        state: 'MA'
+      }
+    }
   }
-
 }
